f89083d2794c48f95e2b0da076740e91f848812e
yunyunz <vanchou1@gmail.com>
2017-05-10 20:24:09 -0400

    Add files via upload
    JavaScript doesn't work. Pic not done yet. Color not change. 
    Check why <!DOCTYPE html> appears several times.

--- CV.js
+++ CV.js
@@ -1,4 +1,7 @@
 
+
 var now = document.getElementById("now");
 now.innerHTML = "To do: insert today's date here ... ";
-
+/*
+var x = new Date(document.lastModified);
+document.getElementById("demo").innerHTML = x;
