aebdd660b997587aa3eea223fbfa9f0930ccd735
yunyunz <vanchou1@gmail.com>
2017-05-11 19:21:56 -0400

    Add files via upload

--- CV.js
+++ CV.js
@@ -2,3 +2,13 @@ function alterdate() {
 var x = new Date(document.lastModified);
 document.getElementById("now").innerHTML = x;
 }
+
+function swap() {
+  if (document.getElementById("picture").src.endsWith('angel.jpg'))  //==:Comparison
+   {
+      document.getElementById("picture").src = "angel2.jpg"; //=:assignment
+   }
+   else
+      document.getElementById("picture").src = "angel.jpg";
+
+}
